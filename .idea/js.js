4f8c3d5ba0e046c7d9c61789c97559b1332c5f71
zx597813039 <597813039@qq.com>
2016-11-23 22:01:15 +0800

    Add

---
+++ .idea/js.js
@@ -0,0 +1,10 @@
+/**
+ * Created by yuchengkai on 2016/11/22.
+ */
+document.write("hello")
+$(document).ready(function () {
+    $("button").addClass("animated bounce");
+    $(".well").addClass("animated shake");
+    $("#target3").addClass("animated fadeOut");
+    $("#target1").css("color", "red");
+})
