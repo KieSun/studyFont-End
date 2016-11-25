b13c3d851248b2100306e1d0ee4237f54eff28d0
zx597813039 <597813039@qq.com>
2016-11-25 23:00:19 +0800

    add

--- .idea/js.js
+++ .idea/js.js
@@ -8,4 +8,20 @@ $(document).ready(function () {
     $("#target3").addClass("animated fadeOut");
     $("#target1").css("color", "blue");
     $("#target1").prop("disabled", true);
+    // 添加标签和文字到元素
+    $("#target4").html("<em>target4</em>");
+    // 移除 HTML 元素
+    $("#target5").remove();
+    // 剪切去别的 ID
+    $("#target2").appendTo("#right-well");
+    // 复制去别的 ID
+    $("#target2").clone().appendTo("#left-well");
+    // 父类
+    $("#target1").parent().css("background-color", "red");
+    // 子类
+    $("#right-well").children().css("color", "orange")
+    // 选择子类中的第二个元素
+    $(".target:nth-child(2)").addClass("animated bounce");
+    // 选择子类中的偶数索引，所以从0开始
+    $(".target:even").addClass("animated shake");
 });
